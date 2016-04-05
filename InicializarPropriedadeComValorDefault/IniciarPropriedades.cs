30abdc1af57211063862d46a9b8252d2183927a2
JeanLLopes <jean.llopes@outlook.com>
2016-04-04 22:19:38 -0300

    Add Folder

---
+++ InicializarPropriedadeComValorDefault/IniciarPropriedades.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InicializarPropriedadeComValorDefault
+{
+    class IniciarPropriedades
+    {
+    }
+
+    public class Produto
+    {
+        //DEFININDO VALOR PADRÃO
+        public int Id { get; set; }
+        public string Nome { get; set; } = "Produto Novo"
+        public decimal Preco { get; set; } = 10
+        public double Quantidade { get; set; }
+
+    }
+}
